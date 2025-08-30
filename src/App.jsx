084af8e1579fddf972cbca85f6d853ca09565d4b
Yuto299 <ota.se.ke@outlook.jp>
2025-08-30 12:29:59 +0900

    三項演算子に書き換えた

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,10 @@ import Login from "./Login";
 
 let isLoggedIn = false;
 
-function renderConditionally() {
-  if (isLoggedIn === true) {
-    return <h1>Hello</h1>;
-  } else {
-    return <Login />;
-  }
-}
-
 function App() {
   return (
     <>
-      <div className="container">{renderConditionally()}</div>
+      <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Login />}</div>
     </>
   );
 }
